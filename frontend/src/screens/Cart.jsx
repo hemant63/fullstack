67f62c2fe9d95228b1c7a267a7fa1942adf485cb
Hemant Pal <130067382+hemant63@users.noreply.github.com>
2025-03-19 18:49:44 +0530

    Delete frontend/src directory

--- frontend/src/screens/Cart.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react'
-
-const Cart = () => {
-  return (
-    <div className="container">Cart</div>
-  )
-}
-
-export default Cart;
--- frontend/src/services/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from axios;
-
-const axiosClient=axios.create({
-    baseURL:'',
-    headers:{
-        'Content-type':'application/json'
-    }
-});
-
